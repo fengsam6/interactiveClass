c7c05a82bf8046a7d07ab9aa578c8615a7125f83
hlmc <1928426652@qq.com>
2020-04-27 09:23:03 +0800

    消息通知组件迁出

--- interactiveClassUI/src/pages/class/notice/index.vue
+++ interactiveClassUI/src/pages/class/notice/index.vue
@@ -1,22 +1,20 @@
 <template>
-    <view>
-        <view v-for="(item,i) in [1,2,3,4]" :key="i">
-            <van-row>
-                <view class="notice_title">
-                    课堂纪律
-                </view>
-                <view class="notice_content">
-                    我是一个好学生， 我是一个好学生， 我是一个好学生， 我是一个好学生。。。
-                </view>
-                <van-col span="10" offset="14">
-                    <view class="notice_foot">
-                        2020-04-10 23:21:51
-                    </view>
-                </van-col>
+    <view v-for="(item,i) in noticeArr" :key="i">
+        <van-row>
+            <view class="notice_title">
+                {{item.noticeTitle}}
+            </view>
+            <view class="notice_content">
+                {{item.noticeContent}}
+            </view>
+            <van-col span="10" offset="14">
+				<view class="notice_foot">
+					{{item.publishTime}}
+				</view>
+            </van-col>
             </van-row>
             <van-divider  custom-style="height:1px"/>
         </view>
-    </view>
 
 </template>
 
